fb4847f347086c9498ad1e8195c2eb5b736cdae6
binhantt <doan44503@gmail.com>
2025-04-22 14:31:18 +0700

    push

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1>Thông tin về chúng tôi</h1>
+      <p>Đây là trang thông tin về chúng tôi.</p>
+    </div>
+  );
+};
+
+export default About;
